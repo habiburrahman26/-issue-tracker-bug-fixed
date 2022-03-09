bebfc4be240fcd48c31dc1822464c5efb5c51cde
HabiburRahmanAnik <habib264521@gmail.com>
2022-03-09 08:38:16 +0600

    fixed close button issue

--- main.js
+++ main.js
@@ -26,7 +26,8 @@ function submitIssue(e) {
 
 const closeIssue = (id) => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find((issue) => issue.id === id);
+  const currentIssue = issues.find((issue) => +issue.id === id);
+
   currentIssue.status = 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
@@ -35,7 +36,7 @@ const closeIssue = (id) => {
 const deleteIssue = (id) => {
   console.log(id);
   const issues = JSON.parse(localStorage.getItem('issues'));
-  // object id is string but parameter recived id is int. so convert object id to int 
+  // object id is string but parameter recived id is int. so convert object id to int
   const remainingIssues = issues.filter((issue) => +issue.id !== id);
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
   fetchIssues();
@@ -55,7 +56,7 @@ const fetchIssues = () => {
                               <h3> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
